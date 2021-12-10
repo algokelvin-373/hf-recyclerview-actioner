7e43ac3795c1fbf887de95433021dac1e3bd52fa
algokelvin-373 <kelvinht373@gmail.com>
2021-12-10 10:50:54 +0700

    [1.0.0] Success Create Method 'Data Adapter' and Implement this recyclerview body

--- recyclerview_demo1/src/main/java/algokelvin/recyclerview/demo1/MainActivity.kt
+++ recyclerview_demo1/src/main/java/algokelvin/recyclerview/demo1/MainActivity.kt
@@ -1,14 +1,29 @@
 package algokelvin.recyclerview.demo1
 
-import algokelvin.actioner.recyclerview.test
-import androidx.appcompat.app.AppCompatActivity
+import algokelvin.actioner.recyclerview.controller.DataAdapter
+import algokelvin.recyclerview.demo1.databinding.ActivityMainBinding
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.item_visitor.view.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
-        test()
+        val data = DataSample.setData()
+        mainBinding.rvItem.layoutManager = LinearLayoutManager(this)
+        val dataAdapter = DataAdapter(data.size, R.layout.item_visitor) { view, position ->
+            view.date_visitor.text = data[position].date
+            view.name_visitor.text = data[position].name
+            view.address_visitor.text = data[position].address
+        }
+        mainBinding.rvItem.adapter = dataAdapter
+        dataAdapter.notifyDataSetChanged()
+
     }
 }
---
+++ recyclerview_demo1/src/main/java/algokelvin/recyclerview/demo1/Visitor.kt
@@ -0,0 +1,8 @@
+package algokelvin.recyclerview.demo1
+
+data class Visitor(
+    val id: Int,
+    val date: String,
+    val name: String,
+    val address: String
+)
